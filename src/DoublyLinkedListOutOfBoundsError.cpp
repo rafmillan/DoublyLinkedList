6d1b03ecccb18a073502c0e1a0169625d6c1b701
Jose Mendoza <joseemendoza194@gmail.com>
2019-12-04 01:42:12 -0800

    Last error file

--- src/DoublyLinkedListOutOfBoundsError.cpp
+++ src/DoublyLinkedListOutOfBoundsError.cpp
@@ -1,7 +1,11 @@
 //
 // Created by mfbut on 11/24/2019.
 //
-
+#include <sstream>
 #include "DoublyLinkedListOutOfBoundsError.h"
 
-
+DoublyLinkedListOutOfBoundsError::DoublyLinkedListOutOfBoundsError():DoublyLinkedListError() {
+  std::stringstream errorStream;
+  errorStream << "Out of Bounds!!!" << std::endl;
+  errorString = errorStream.str();
+}
