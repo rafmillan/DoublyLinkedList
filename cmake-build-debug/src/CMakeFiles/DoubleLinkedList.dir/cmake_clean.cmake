f0465efb535e2b03fd5064c587a36ecfcdfb7d03
Jose Mendoza <joseemendoza194@gmail.com>
2019-12-04 15:27:26 -0800

    Everything

---
+++ cmake-build-debug/src/CMakeFiles/DoubleLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DoubleLinkedList.dir/DoublyLinkedListError.cpp.o"
+  "CMakeFiles/DoubleLinkedList.dir/DoublyLinkedListEmptyError.cpp.o"
+  "CMakeFiles/DoubleLinkedList.dir/DoublyLinkedListOutOfBoundsError.cpp.o"
+  "libDoubleLinkedList.pdb"
+  "libDoubleLinkedList.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DoubleLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
